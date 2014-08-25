7c814820c15594dd7072a335a8e499b98da699dd
Venkat <venkat@ridecell.com>
2014-08-25 00:41:13 -0700

    fixed schedule ordering

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20140824051442) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "holidays", force: true do |t|
     t.date     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "holidays", ["data"], name: "index_holidays_on_data", unique: true
+  add_index "holidays", ["data"], name: "index_holidays_on_data", unique: true, using: :btree
 
   create_table "off_days", force: true do |t|
     t.date     "date"
@@ -28,8 +31,8 @@ ActiveRecord::Schema.define(version: 20140824051442) do
     t.datetime "updated_at"
   end
 
-  add_index "off_days", ["date"], name: "index_off_days_on_date", unique: true
-  add_index "off_days", ["user_id"], name: "index_off_days_on_user_id"
+  add_index "off_days", ["date"], name: "index_off_days_on_date", unique: true, using: :btree
+  add_index "off_days", ["user_id"], name: "index_off_days_on_user_id", using: :btree
 
   create_table "order_entries", force: true do |t|
     t.integer  "order"
@@ -38,7 +41,7 @@ ActiveRecord::Schema.define(version: 20140824051442) do
     t.datetime "updated_at"
   end
 
-  add_index "order_entries", ["user_id"], name: "index_order_entries_on_user_id"
+  add_index "order_entries", ["user_id"], name: "index_order_entries_on_user_id", using: :btree
 
   create_table "scheduled_till_dates", force: true do |t|
     t.date     "date"
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 20140824051442) do
     t.datetime "updated_at"
   end
 
-  add_index "scheduled_till_dates", ["date"], name: "index_scheduled_till_dates_on_date", unique: true
+  add_index "scheduled_till_dates", ["date"], name: "index_scheduled_till_dates_on_date", unique: true, using: :btree
 
   create_table "schedules", force: true do |t|
     t.date     "date"
@@ -55,8 +58,8 @@ ActiveRecord::Schema.define(version: 20140824051442) do
     t.datetime "updated_at"
   end
 
-  add_index "schedules", ["date"], name: "index_schedules_on_date", unique: true
-  add_index "schedules", ["user_id"], name: "index_schedules_on_user_id"
+  add_index "schedules", ["date"], name: "index_schedules_on_date", unique: true, using: :btree
+  add_index "schedules", ["user_id"], name: "index_schedules_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name",       limit: 50
@@ -64,6 +67,6 @@ ActiveRecord::Schema.define(version: 20140824051442) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["name"], name: "index_users_on_name", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
 
 end
